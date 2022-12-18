8f746302c608d90260cf96cc9f3d658bbf2341f9
Sirius Black <mariia.beliaev@gmail.com>
2022-12-18 23:57:59 +0300

    added answers

--- task1/index.js
+++ task1/index.js
@@ -1,6 +1,11 @@
-const checkValidity = (value) => {
-  if (value === "") {
+let info = document.querySelectorAll(".input");
+let button = document.getElementsByClassName(".questionnaire__btn");
+
+const checkValidity = () => {
+  if (info.value === "") {
     alert("Поля не заполнены");
   }
   return true;
 };
+
+button.addEventListener("click", checkValidity);
